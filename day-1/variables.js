9cda5b703e1a4a5f74a4309df662b19149621acc
Nouman Coding <noumanali.coding@gmail.com>
2025-06-30 13:40:46 +0500

    Check and log data types of variables

--- day-1/variables.js
+++ day-1/variables.js
@@ -1,7 +1,11 @@
 let fullName = "Nouman";
-let age = "18";
-let isStudents = "Yes";
+let age = 18;
+let isStudent = true;
 
 console.log("Name:", fullName);
 console.log("Age:", age);
 console.log("Student?", isStudent);
+
+console.log(typeof fullName);   // string
+console.log(typeof age);        // number
+console.log(typeof isStudent);  // boolean
