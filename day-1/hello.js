3999c536ec1efdf11da2184abe21575c84696ade
Nouman Coding <noumanali.coding@gmail.com>
2025-06-30 13:30:52 +0500

    feat: add hello.js basic console log

---
+++ day-1/hello.js
@@ -0,0 +1,5 @@
+// What is JavaScript?
+// - JavaScript is the programming language of the Web.
+// - It adds logic, interactivity, and power to HTML and CSS.
+
+console.log("Hello — JavaScript! I am learning.");
